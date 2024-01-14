b8bfc1eba423981c0f000a2de0f0f2c08416ddf8
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-01-14 14:25:17 +0530

    add databass

--- server/index.js
+++ server/index.js
@@ -40,7 +40,9 @@ async function run() {
         res.send(result);
       })
 
-       
+       //get all books from the database 
+          app.get
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
